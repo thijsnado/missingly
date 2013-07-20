aa6ea37ca271a6f7f520239842f46f603eaddcbe
Thijs de Vries <tdevries@patientslikeme.com>
2013-07-19 23:31:11 -0400

    Remove a guard that is not needed.

--- lib/missingly/delegate_matcher.rb
+++ lib/missingly/delegate_matcher.rb
@@ -1,16 +1,12 @@
 module Missingly
   class DelegateMatcher
     def handle(instance, name, *args, &block)
-      if should_respond_to?(instance, name)
-        instance.class.class_eval <<-RUBY, __FILE__, __LINE__
-          def #{name}(*args, &block)
-            #{delegate_name}.#{name}(*args, &block)
-          end
-        RUBY
-        instance.public_send(name, *args, &block)
-      else
-        super
-      end
+      instance.class.class_eval <<-RUBY, __FILE__, __LINE__
+        def #{name}(*args, &block)
+          #{delegate_name}.#{name}(*args, &block)
+        end
+      RUBY
+      instance.public_send(name, *args, &block)
     end
   end
 end
