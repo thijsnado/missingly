0f270af4b9d3e4d8e8aa24e058fc2806db0a090f
Thijs de Vries <tdevries@patientslikeme.com>
2013-07-23 21:28:17 -0400

    Adding some failing tests to class methods.

--- spec/class_inheritance_spec.rb
+++ spec/class_inheritance_spec.rb
@@ -45,4 +45,39 @@ describe Missingly::Matchers do
       subclass_with_overrides.new.foo.should eq :super_duper
     end
   end
+
+  describe "overriding class methods" do
+    let(:super_class) do
+      Class.new do
+        include Missingly::Matchers
+
+        handle_missingly [:foo], class_method: true do |method|
+          return method
+        end
+      end
+    end
+
+    let(:subclass_with_overrides) do
+      Class.new(super_class) do
+        handle_missingly [:foo], class_method: true do |_|
+          :super_duper
+        end
+      end
+    end
+
+    it "should work when called before parent" do
+      subclass_with_overrides.foo.should eq :super_duper
+    end
+
+    it "should work when called after parent" do
+      super_class.foo
+      subclass_with_overrides.foo.should eq :super_duper
+    end
+
+    it "should work when subclass initiated before parent method defined" do
+      subclass_with_overrides
+      super_class.foo
+      subclass_with_overrides.foo.should eq :super_duper
+    end
+  end
 end
